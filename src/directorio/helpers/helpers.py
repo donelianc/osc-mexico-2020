ef208fcba107a620739f33cdbc26aad1448d7549
Ian Castillo <ian.castillo@nubank.com.br>
2021-03-17 16:52:51 -0600

    unify logic to use in/out extension in directorios

--- src/directorio/helpers/helpers.py
+++ src/directorio/helpers/helpers.py
@@ -1,19 +1,23 @@
-from pathlib import Path
 from glob import glob
+from pathlib import Path
 from pandas import to_datetime
+from requests.exceptions import ReadTimeout
 from helpers.get_clunis_from_sirfosc import *
 from helpers.get_donauts_from_sat import *
 from helpers.get_donauts_from_dof import *
 
 
-def check_sources(from_source, path, extension, log):
+def check_sources(from_source, dir_path, to_extension, log, year=""):
 
     log.info("Checking if directorio already exists")
-    assert path[-1] == "/", "make sure path ends with /"
-    assert extension[0] == ".", "make extension starts with ."
+    assert dir_path[-1] == "/", "make sure path ends with /"
 
-    Path(path).mkdir(parents=True, exist_ok=True)
-    full_path = path + "*" + extension
+    if year != "":
+        year = f"{year}/"
+
+    dir_path += f"{to_extension}/{year}"
+    Path(dir_path).mkdir(parents=True, exist_ok=True)
+    full_path = f"{dir_path}*.{to_extension}"
 
     if from_source:
         log.info("Directory will be created from source")
@@ -28,7 +32,7 @@ def check_sources(from_source, path, extension, log):
     return fs
 
 
-def get_source(source, fs, path, extension, log):
+def get_source(source, fs, dir_path, from_extension, to_extension, log, year=""):
 
     log.info("Looking for sources created prev")
     assert source in [
@@ -38,13 +42,14 @@ def get_source(source, fs, path, extension, log):
     ], "Only ['clunis', 'donauts-sat', 'donauts-dof'] are available"
     assert isinstance(fs, list), "files isn't a list"
 
-    n = len(fs)
+    dir_path += f"{to_extension}/"
 
-    if n == 1:
+    if len(fs) == 1:
         log.info("One directory found, loading data from it")
-        file = str(fs[0])
+        csv_path = str(fs[0])
+        df = read_csv(csv_path, low_memory=False)
 
-    elif n > 1:
+    elif len(fs) > 1:
         log.info("Multiple directories found, loading data from most recent")
         dts = dict(
             [(dt, to_datetime(dt[-23:-4], format="%Y-%m-%d-%H-%M-%S")) for dt in fs]
@@ -52,29 +57,29 @@ def get_source(source, fs, path, extension, log):
         dates = list(dts.values())
         names = list(dts.keys())
 
-        file = names[dates.index(max(dts.values()))]
+        csv_path = names[dates.index(max(dts.values()))]
+        df = read_csv(csv_path, low_memory=False)
 
     else:
         log.info("No sources found, downloading from original")
         try:
+            dir_path = dir_path.replace(to_extension, from_extension)
             if source == "clunis":
-                df, now = get_clunis_from_sirfosc(path, extension)
-                file = f"{path}{extension[1:]}/{now}{extension}"
+                df, file_path = get_clunis_from_sirfosc(dir_path, log=log)
             elif source == "donauts-sat":
-                df, now = get_donauts_from_sat(path[-5:-1], path)
-                file = f"{path}{now}{extension}"
+                df, file_path = get_donauts_from_sat(year=year, path=dir_path, log=log)
             elif source == "donauts-dof":
-                df, now = get_donauts_from_dof(path[-5:-1], path)
-                file = f"{path}{now}{extension}".replace("pdf", extension[1:])
+                df, file_path = get_donauts_from_dof(year=year, path=dir_path, log=log)
 
         except Exception as e:
-            log.error("something occurred when talking to the server")
-            raise (e)
+            raise e
 
         try:
-            df.to_csv(file)
-            log.info(f"Data loaded from response, saved at: {file}")
+            csv_path = file_path.replace(from_extension, to_extension)
+            df.to_csv(csv_path)
+            log.info(f"Data loaded from response, original saved at: {file_path}")
         except Exception as e:
             log.error("directory creation (csv) failed")
+            raise e
 
-    return file
+    return df, csv_path
